e95b106b6b58d717c70c25ba50c7c1fcd2896a7f
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2023-05-24 21:05:41 +0100

    Support for relative paths to the entry points. (reverted script overwritten by mistake)

--- index.js
+++ index.js
@@ -1,192 +1,124 @@
-#! /usr/bin/env node
 "use strict";
-// https://github.com/kingsdigitallab/webval/blob/main/docs/utils.js
-// TODO: rewrite all the paths in the responses (nav>@id, dts:passage, dts:references)
-// TODO: error management!
-// TODO: sync -> async
-//
 
-const dtsutils = require("kdl-dts-client");
-const { exit } = require("process");
-const path = require("path");
-const fs = require("fs");
+// PLEASE keep the code both browser and server compatible.
+// Make sure dependencies work in both environments.
 
-class Generator {
-  constructor() {
-    this.loadSettings()
-  }
-  loadSettings() {
-    let ret = require("../settings")
-    // read service settings from settings.js .
-    // they can be overridden by a json file passed as the last argument
-    let path = process.argv[process.argv.length - 1]
-    if (path.endsWith(".json")) {
-      let content = fs.readFileSync(path)
-      ret = {
-        ...ret,
-        ...JSON.parse(content)
-      }
+(function (exports) {
+  exports.fetchDTS = async function (panel, service, id, ref, format) {
+    let url = getDTSUrl(panel, service, id, ref, format);
+    return await fetchContent(url, getFormatFromRequest(service, format));
+  };
+
+  function getFormatFromRequest(service, format) {
+    let ret = "json";
+    if (service == "documents") {
+      ret = format || "tei";
     }
-
-    if (!ret.local) {
-      // if not provided, the local destination is the filename of the target
-      ret.local = ret.target.replace(/^.*\//, '')
-    }
-
-    this.settings = ret
-
-    return ret
+    return ret;
   }
-  async generate() {
-    this.validateSettings();
+  exports.getFormatFromRequest = getFormatFromRequest;
 
-    if (this.settings.clear) this.clearLocalFolder();
+  function slugify(s) {
+    return s.replace(new RegExp("[^\\w.]+", "g"), "-");
+  }
 
-    // get services URIs from DTS entrypoint
-    this.responses = {
-      entryPoint: await this.fetchAndWriteDTS(),
-    };
+  function getDTSUrl(panel, service, id, ref, format) {
+    // The absolute path (URL or filesystem) to a DTS entry point
+    let ret = panel.selections.source;
+    if (!service) return ret;
+    let isStatic = ret.endsWith(".json");
+    ret = ret.replace(".json", "");
 
-    // TODO: rewrite webpaths of the services
-    // this.responses.collections = `${this.target}/collections`
+    let entryPoint = panel.responses.entryPoint[service]
+    let isRelative = !entryPoint.startsWith('/') && !entryPoint.startsWith('http')
+    if (isRelative) {
+      // If panel.responses.entryPoint[service] is a relative path (i.e. not http(s):// or /X)
+      // then we don't truncate ret first.
+      // This way we can relocate the same static files to different URL and they will still work.
+      // e.g. https://raw.githubusercontent.com/kingsdigitallab/alice-thornton/dts/dts.json
+      // + dts/collections
+      // => https://raw.githubusercontent.com/kingsdigitallab/alice-thornton/dts/dts/collections
+      ret = ret.replace(/[^\/]*$/, '')
+    } else {
+      // e.g. https://raw.githubusercontent.com/kingsdigitallab/alice-thornton/dts/dts.json
+      // + /dts/collections
+      // => https://raw.githubusercontent.com/dts/collections
+      ret = getServiceRootFromSource(ret);
+    }
+    ret = `${ret}${entryPoint}`;
 
-    // get root collection
-    // TODO: paginate
-    let res = await this.fetchAndWriteDTS("collections");
-
-    // TODO: get subcollections (recursive fct)
-
-    // get nav/documents
-    for (let colMember of res.member) {
-      if (colMember["@type"] == "Resource") {
-        // get Navigation
-        // TODO: paginate
-        let nav = await this.fetchAndWriteDTS("navigation", colMember["@id"]);
-
-        // get Passages
-        for (let ref of nav.member) {
-          for (let format of this.settings.formats) {
-            await this.fetchAndWriteDTS(
-              "documents",
-              colMember["@id"],
-              ref["dts:ref"],
-              format
-            );
-          }
-          break;
+    if (isStatic) {
+      ret = ret.replace(/\/$/, "");
+      if (service) {
+        if (id) {
+          ret += `/${slugify(id)}`;
+        }
+        if (ref) {
+          ret += `/${slugify(ref)}`;
         }
       }
-    }
 
-    console.log("done.");
-  }
-  clearLocalFolder() {
-    let apath = path.resolve(this.settings.local.replace(".json", ""));
-    if (fs.existsSync(apath)) {
-      console.log("  RMDIR " + apath);
-      fs.rmSync(apath, { recursive: true });
-    }
-  }
-  async fetchAndWriteDTS(service, id, ref, format) {
-    let ret = await dtsutils.fetchDTS(
-      {
-        selections: { source: this.settings.source },
-        responses: this.responses,
-      },
-      service,
-      id,
-      ref,
-      format
-    );
-    if (!ret)
-      this.error(`DTS request failed. (${service}, ${id}, ${ref}, ${format})`);
-
-    let res = this.getTranformedResponse(service, ret);
-
-    this.writeResponse(res, service, id, ref, format);
-
-    return ret;
-  }
-  getParentFolderName() {
-    return new URL(this.settings.target)
-      .pathname
-      .replace(/^.*\//, "")
-      .replace(/\.json$/, "")
-  }
-  getTranformedResponse(service, res) {
-    if (service == "documents") return res;
-
-    let ret = JSON.parse(JSON.stringify(res));
-    if (!service) {
-      let targetRoot = new URL(this.settings.target).pathname.replace(
-        /\.json$/,
-        ""
-      );
-      let parentFolder = this.getParentFolderName()
-      ret["@id"] = `${targetRoot}.json`;
-      ret.collections = `${parentFolder}/collections`;
-      ret.navigation = `${parentFolder}/navigation`;
-      ret.documents = `${parentFolder}/documents`;
-    }
-    return ret;
-  }
-  writeResponse(data, service, id, ref, format) {
-    let parentFolder = this.getParentFolderName()
-    let filePath = dtsutils.getDTSUrl(
-      {
-        selections: { source: this.settings.local },
-        responses: {
-          entryPoint: {
-            collections: `${parentFolder}/collections`,
-            navigation: `${parentFolder}/navigation`,
-            documents: `${parentFolder}/documents`,
-          },
-        },
-      },
-      service,
-      id,
-      ref,
-      format
-    );
-    if (dtsutils.getFormatFromRequest(service, format) == "json") {
-      data = JSON.stringify(data, null, 1);
-    }
-    let parentPath = path.dirname(filePath);
-    fs.mkdirSync(parentPath, { recursive: true });
-
-    console.log("  WRITE " + path.resolve(filePath));
-    fs.writeFileSync(filePath, data, "utf8");
-  }
-  validateSettings() {
-    let errors = [];
-    let s = this.settings;
-    let localParent = path.dirname(path.resolve(this.settings.local));
-    if (!fs.existsSync(localParent)) {
-      errors.push("settings.local parent path does not exist");
-    }
-    if (localParent == "/") {
-      errors.push("settings.local cannot be a root folder");
-    }
-    if (!s.local.match(/\w+\.json$/)) {
-      errors.push("settings.local must end with a valid .json filename");
-    }
-    if (!s.target.endsWith(".json")) {
-      errors.push('settings.target must end with extension ".json"');
-    }
-    if (errors.length) {
-      for (let error of errors) {
-        console.error(error);
+      ret += `.${getFormatFromRequest(service, format)}`;
+    } else {
+      ret += `?`;
+      if (id) {
+        ret += `&id=${id}`;
       }
-      exit(1);
+      if (ref) {
+        ret += `&ref=${ref}`;
+      }
+      if (service == "documents" && format && format != "tei") {
+        ret += `&format=${format}`;
+      }
     }
-  }
-  logjson(data) {
-    console.log(JSON.stringify(data, null, 2));
-  }
-  error(message) {
-    console.error(`ERROR: ${message}`);
-    exit(1);
-  }
-}
 
-new Generator().generate();
+    console.log(panel)
+    console.log(service)
+    console.log(ret)
+
+    return ret;
+  }
+  exports.getDTSUrl = getDTSUrl;
+
+  function getServiceRootFromSource(str) {
+    // The address to which service paths are relative.
+    // returns the hostname if URL
+    // returns str otherwise
+    let ret = str;
+    let url;
+    try {
+      url = new URL(str);
+    } catch (_) {
+      //
+    }
+    if (url && (url.protocol === "http:" || url.protocol === "https:")) {
+      ret = url.origin;
+    }
+    return ret;
+  }
+
+  async function fetchContent(url, format) {
+    let ret = null;
+
+    let gfetch = null;
+    if (typeof fetch == "undefined") {
+      gfetch = require("node-fetch");
+    } else {
+      gfetch = fetch;
+    }
+
+    console.log(`  FETCH ${url}`);
+
+    let res = await gfetch(url);
+    if (res && res.status == 200) {
+      if (!format || format == "json") {
+        ret = await res.json();
+      } else {
+        ret = await res.text();
+      }
+    }
+    return ret;
+  }
+  exports.fetchContent = fetchContent
+
+})(typeof exports === "undefined" ? (this["dtsutils"] = {}) : exports);
